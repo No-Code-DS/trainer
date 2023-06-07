ab96e909e0ec934d9a75cf960b029fa0a9a700f6
Cine-Pixel <ikaumphriani@gmail.com>
2023-06-07 16:47:20 +0400

    feat(NP-44): sample model training and save

--- trainer/consumer.py
+++ trainer/consumer.py
@@ -1,44 +1,72 @@
 import json
+import os
 import pickle
 
 import pandas as pd
 import pika
-from sklearn.metrics import confusion_matrix
+from models import SelectedModel, SessionLocal, StatusEnum
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ["RABBIT_URL"]))
 
 channel = connection.channel()
 
 channel.queue_declare(queue="training")
 
 
+def prepare_data(file_path: str, prediction_field: str):
+    X = pd.read_csv("../" + file_path)
+    X = X.select_dtypes(["number", "bool"])
+    y = X[prediction_field]
+    X = X.drop(prediction_field, axis=1)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
 def callback(ch, method, properties, body):
     print("consumed in trainer")
     config = json.loads(body)
+    print(config)
 
-    X = pd.read_csv(f"upload/data/ready/{config.filename}")
-    y = X[config["prediction_field"]]
-    X = X.drop(config["prediction_field"], axis=1)
+    X_train, X_test, y_train, y_test = prepare_data(config["file_path"], config["prediction_field"])
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+    db = SessionLocal()
 
-    if config["model"] == "LinearRegression":
-        print("train linear regression")
-        from sklearn.linear_model import LinearRegression
+    if config["name"] == "LinearRegression":
+        print("Training linear regression")
 
-        model = LinearRegression(fit_intercept=config["fit_intercept"], positive=config["positive"])
+        model = LinearRegression(
+            fit_intercept=config["params"]["fit_intercept"], positive=config["params"]["positive"]
+        )
         model.fit(X_train, y_train)
 
         y_pred = model.predict(X_test)
-        matrix = confusion_matrix(y_test, y_pred)
-        print(matrix)
-        with open("trained_models/model.pkl", "wb") as pkl:
+        evaluations = {
+            "mae": mean_absolute_error(y_test, y_pred),
+            "mse": mean_squared_error(y_test, y_pred),
+        }
+        print(evaluations)
+
+
+        db_model = db.query(SelectedModel).get(config["model_id"])
+        db_model.evaluation = json.dumps(evaluations)
+        db_model.status = StatusEnum.TRAINED
+
+        db.commit()
+
+        with open(f"../trained_models/linear_regression_{config['model_id']}.pkl", "wb") as pkl:
             pickle.dump(model, pkl)
-    elif config["model"] == "RandomForestRegressor":
-        print("train random forest regressor")
-    elif config["model"] == "RandomForestClassifier":
-        print("train random forest classifier model")
+
+    elif config["name"] == "LogisticRegression":
+        # matrix = confusion_matrix(y_test, y_pred)
+        print("Training random logistic regression")
+    elif config["name"] == "RandomForestRegressor":
+        print("Training random forest regressor")
+    elif config["name"] == "RandomForestClassifier":
+        print("Training random forest classifier model")
     else:
         print("Unknown model...")
 
